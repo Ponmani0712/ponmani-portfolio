6b0e1c7332b31b1d65c8b52d6d75736c703ff791
Ponmani T <ponmaniu202469@gmail.com>
2026-01-24 22:31:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const aboutSection = document.querySelector('.about');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, { threshold: 0.3 });
+
+observer.observe(aboutSection);
